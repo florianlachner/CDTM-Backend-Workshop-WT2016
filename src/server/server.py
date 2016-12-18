16424fde0a506fdf876152256be77ebf39eb3008
Florian Lachner <florian.lachner@ifi.lmu.de>
2016-12-18 09:23:56 +0100

    no message

--- src/server/server.py
+++ src/server/server.py
@@ -13,7 +13,7 @@ from list import List
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-VERSION = 4.0
+VERSION = 6.0
 
 myLists = [
     List('Inbox', id='0'),
@@ -105,17 +105,19 @@ def remove_task(list_id, task_id):
 
     return jsonify({'result': True})
 
-# UDPATE ROUTE
-
-@app.route('/api/lists/<string:list_id>/tasks', methods=['PUT'])
-def update_task(list_id):
-    ''' creates a new task for a list '''
-
+# UPDATE ROUTE
+@app.route('/api/lists/<string:list_id>/tasks/<string:task_id>', methods=['PUT'])
+def update_task(list_id, task_id):
     # 1. Check whether the specified list exists
     if (len([l for l in myLists if l.id == list_id]) < 1):
         json_abort(404, 'List not found')
 
-    # 2. Check whether the required parameters have been sent
+    # 2. Check whether the specified task exists
+    tasks = [t for t in myTasks if t.id == task_id and t.list == list_id]
+    if (len(tasks) < 1):
+        json_abort(404, 'Task not found')
+
+    # 3. Check whether the required parameters have been sent
     try:
          data = request.get_json()
     except:
@@ -125,18 +127,26 @@ def update_task(list_id):
         json_abort(400, 'Invalid Content-Type')
 
     title = data.get('title', None)
-    if title == None:
+    status = data.get('status', None)
+    description = data.get('description', None)
+    due = data.get('due', None)
+    revision = data.get('revision', None)
+
+    if title == None or status == None or description == None or \
+    due == None or revision == None:
         json_abort(400, 'Invalid request parameters')
 
-    # 3. calculate the next id
-    id = max([int(t.id) for t in myTasks]+[-1]) + 1
-    newTask = Task(title, list_id, id=str(id), status = Task.NORMAL)
+    if revision < tasks[0].revision:
+        json_abort(409, 'Newer version of task available')
 
-    # 4. append task to array
-    myTasks.append(newTask)
+    # TODO: ignoring 'list' for now. Implement moving tasks from one list to another
+    tasks[0].title = title
+    tasks[0].status = status
+    tasks[0].description = description
+    tasks[0].due = due
+    tasks[0].revision = tasks[0].revision + 1
 
-    # 5. return new task
-    return jsonify(newTask.__dict__)
+    return jsonify(tasks[0].__dict__)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=20006, debug=True)
